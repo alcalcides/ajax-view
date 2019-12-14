c678cd60bd7675d8e2bb772dd55fd32560770d27
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2019-12-13 23:55:03 -0300

    NPM install ok

---
+++ comandos.sh
@@ -0,0 +1,12 @@
+#node index.js 
+#git status
+#git diff index.js
+#heroku login
+#heroku login -i
+#heroku git:remote -a ajax-view
+#git remote -v
+#git commit -m "Deploy Heroku"
+#git commit -am "Fixs port for Heroku"
+#git push github master
+#git push heroku master
+#npm install
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+var PORT = process.env.PORT || 8080
 var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
-}).listen(443);
+}).listen(PORT);
+console.log('Server running at port ' + PORT);
