a3206e41a2d3b62b1a6bb433bdbae8781b42ad64
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2019-12-14 14:41:57 -0300

    Simplifies packages config files

--- comandos.sh
+++ comandos.sh
@@ -1,12 +1,16 @@
+#npm init
+#npm install
 #node index.js 
-#git status
-#git diff index.js
+#npm start
+#npm install --dev <xxx>
 #heroku login
 #heroku login -i
 #heroku git:remote -a ajax-view
+git status
+#git diff index.js
 #git remote -v
-#git commit -m "Deploy Heroku"
-#git commit -am "Fixs port for Heroku"
-#git push github master
-#git push heroku master
-#npm install
+git add .
+git status
+git commit -m "Simplifies packages config files"
+git push github master
+git push heroku master
