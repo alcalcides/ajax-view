aaa0ba76cbd90cd0fb835da8d957763ddaa1247f
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2019-12-19 11:11:15 -0300

    Prepares netlify environment

--- commands.sh
+++ commands.sh
@@ -3,13 +3,14 @@
 #node index.js 
 #npm start
 #npm install nodemon -D
+#npm install -g netlify-cli
 #heroku login
-heroku login -i
+#heroku login -i
 #heroku git:remote -a ajax-view
 #git status
 #git diff index.js
 #git remote -v
 git add .
-git commit -m "HTML application prototype"
-git push github master
-git push heroku master
+git commit -m "Prepares netlify environment"
+git push github prototypeHTML
+#git push heroku master
